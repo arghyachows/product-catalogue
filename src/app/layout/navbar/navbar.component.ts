e2894b6bad63b59ec386aa3dfef10952e4cfd1c9
Arghya Chowdhury <arghyachowdhury2610@gmail.com>
2020-07-04 04:07:59 +0530

    Title Change

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-  title = 'Product Catalogue';
+  title = 'Sale Kart';
   constructor() { }
 
   ngOnInit() {
