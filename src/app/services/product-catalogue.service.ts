67efd821fb4af3788b0d8f6b6e6b6b5bb6a08688
arghyachowdhury2610@gmail.com <arghya@gintaa.com>
2021-05-15 06:38:36 +0530

    IP Change

--- src/app/services/product-catalogue.service.ts
+++ src/app/services/product-catalogue.service.ts
@@ -1,53 +1,56 @@
 /*product-catalogue.service.ts */
-import { Injectable } from '@angular/core';
-import { Product } from '../model/product';
-import { Http, Headers } from '@angular/http';
+import { Injectable } from "@angular/core";
+import { Product } from "../model/product";
+import { Http, Headers } from "@angular/http";
 
 @Injectable()
 export class ProductCatalogueService {
+  // The project uses InMemoryWebApi to handle the Server API.
+  // Here "api/productList" simulates a Server API url
+  private productListUrl = "40.76.153.108:3000/api/products";
+  private headers = new Headers({ "Content-Type": "application/json" });
+  constructor(private http: Http) {}
 
-	// The project uses InMemoryWebApi to handle the Server API. 
-	// Here "api/productList" simulates a Server API url 
-	private productListUrl = "192.168.99.100:3000/api/products";
-	private headers = new Headers({ 'Content-Type': "application/json" });
-	constructor(private http: Http) { }
+  // getProductList() performs http.get() and returns a promise
+  public getProductList(): Promise<any> {
+    return this.http
+      .get(this.productListUrl)
+      .toPromise()
+      .then((response) => response.json())
+      .catch(this.handleError);
+  }
 
-	// getProductList() performs http.get() and returns a promise
-	public getProductList(): Promise<any> {
-		return this.http.get(this.productListUrl)
-			.toPromise()
-			.then(response => response.json())
-			.catch(this.handleError);
-	}
+  //addProduct() creates new products
+  public addProduct(product: Product): Promise<any> {
+    return this.http
+      .post(this.productListUrl, JSON.stringify(product), {
+        headers: this.headers,
+      })
+      .toPromise()
+      .then((response) => response.json())
+      .catch(this.handleError);
+  }
+  //updateProduct() creates new products
+  public updateProduct(product: Product): Promise<any> {
+    const url = `${this.productListUrl}/${product.id}`;
+    return this.http
+      .put(url, JSON.stringify(product), { headers: this.headers })
+      .toPromise()
+      .then(() => product)
+      .catch(this.handleError);
+  }
+  //deleteProduct() creates new products
+  public deleteProduct(product: Product): Promise<void> {
+    const url = `${this.productListUrl}/${product.id}`;
+    return this.http
+      .delete(url, { headers: this.headers })
+      .toPromise()
+      .then(() => null)
+      .catch(this.handleError);
+  }
 
-	//addProduct() creates new products 
-	public addProduct(product: Product): Promise<any> {
-		return this.http.post(this.productListUrl, JSON.stringify(product), { headers: this.headers })
-			.toPromise()
-			.then(response => response.json())
-			.catch(this.handleError);
-	}
-	//updateProduct() creates new products 
-	public updateProduct(product: Product): Promise<any> {
-
-		const url = `${this.productListUrl}/${product.id}`;
-		return this.http.put(url, JSON.stringify(product), { headers: this.headers })
-			.toPromise()
-			.then(() => product)
-			.catch(this.handleError);
-	}
-	//deleteProduct() creates new products 
-	public deleteProduct(product: Product): Promise<void> {
-		const url = `${this.productListUrl}/${product.id}`;
-		return this.http.delete(url, { headers: this.headers })
-			.toPromise()
-			.then(() => null)
-			.catch(this.handleError);
-	}
-
-
-	private handleError(error: any): Promise<any> {
-		console.error('An error occurred', error);
-		return Promise.reject(error.message || error);
-	}
+  private handleError(error: any): Promise<any> {
+    console.error("An error occurred", error);
+    return Promise.reject(error.message || error);
+  }
 }
